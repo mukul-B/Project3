a5ca55f6f1505bce1c9ef00a180cd0edac97e1fe
muku <mukulbadhan@gmail.com>
2021-11-02 01:05:19 -0700

    getting some change in plot

--- test_script.py
+++ test_script.py
@@ -4,6 +4,7 @@ import neural_network as nn
 import matplotlib.pyplot as plt
 
 # Generate a dataset and plot it
+plt.close()
 np.random.seed(0)
 X, y = make_moons(200, noise=0.20)
 plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
@@ -25,12 +26,12 @@ def plot_decision_boundary(pred_func, X, y):
 
 
 plt.figure(figsize=(16, 32))
-X=X[:5]
-y=y[:5]
-hidden_layer_dimensions = [3]
+# X=X[:5]
+# y=y[:5]
+hidden_layer_dimensions = [2,3,4]
 for i, nn_hdim in enumerate(hidden_layer_dimensions):
     plt.subplot(5, 2, i+1)
     plt.title('HiddenLayerSize%d' % nn_hdim)
     model = nn.build_model(X, y, nn_hdim)
     plot_decision_boundary(lambda x: nn.predict(model, x), X, y)
-#plt.show()
+plt.show()
