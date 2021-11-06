7f460954c03a04a2db9d49754f8dda0ae79bd589
mukul-B <mukulbadhan@gmail.com>
2021-11-05 17:15:17 -0700

    started batch processing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import numpy as np
 
+b2 = np.random.normal(size=100)
+for i in range(0,len(b2),10):
 
-for i in range(10):
-    print("idris",i)
+    print("idris",b2[i:i+10])
--- neural_network.py
+++ neural_network.py
@@ -25,18 +25,16 @@ def calculate_loss(model, X, y):
 
 
 def predict(model, X):
-    y_pred = np.zeros_like(X)
-    for x in range(len(X)):
-        A = np.matmul(X[x], model['W1']) + model['b1']
-        H = tanhVec(A)
-        z = np.matmul(H, model['W2']) + model['b2']
-        y_pred[x] = z
-    y_res = np.array([1 if (y_pred[i][1] > y_pred[i][0]) else 0 for i in range(len(X))])
+    A = np.matmul(X, model['W1']) + model['b1']
+    H = tanhVec(A)
+    z = np.matmul(H, model['W2']) + model['b2']
+    # y_pred = np.array([np.squeeze(softmax((z[i])[np.newaxis])) for i in range(len(X))])
+    y_res = np.array([1 if (z[i][1] > z[i][0]) else 0 for i in range(len(X))])
     return y_res
 
 
 def random_model(input_size, nn_hdim):
-    # np.random.seed(1)
+    np.random.seed(1)
     w1 = np.random.normal(size=(input_size, nn_hdim))
     b1 = np.zeros(nn_hdim)
     w2 = np.random.normal(size=(nn_hdim, input_size))
@@ -45,34 +43,38 @@ def random_model(input_size, nn_hdim):
     return model
 
 
-def build_model(X, y, nn_hdim, num_passes=1, print_loss=False):
+def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
     input_size = len(X[0])
     output_size = len(X[0])
     model = random_model(input_size, nn_hdim)
     onehot_encoder = OneHotEncoder(sparse=False)
     y_vector = onehot_encoder.fit_transform(y.reshape(len(y), 1))
-
+    batch_size = 20
     for passes in range(num_passes):
         loss = 0
         y_pred = np.zeros_like(X)
 
-        for x in range(len(X)):
-            A = np.matmul(X[x], model['W1']) + model['b1']
+        for i in range(0, len(X), batch_size):
+            batch_x = X[i:i + batch_size]
+            batch_y = y_vector[i:i + batch_size]
+            A = np.matmul(batch_x, model['W1']) + model['b1']
             H = tanhVec(A)
             z = np.matmul(H, model['W2']) + model['b2']
-            y_pred[x] = np.squeeze(softmax(z[np.newaxis]))
+            y_pred = np.array([np.squeeze(softmax((z[i])[np.newaxis])) for i in range(len(batch_x))])
 
-            for l in range(output_size):
-                if y_pred[x][l] != 0.0:
-                    loss = (y_vector[x][l] * math.log(y_pred[x][l]))
-            # print(loss)
-            # print(model)
-            model = updateModel(model, A, H, X[x], y_pred[x], y_vector[x])
+            # for l in range(output_size):
+            #     if y_pred[x][l] != 0.0:
+            #         loss = (y_vector[x][l] * math.log(y_pred[x][l]))
+            # # print(loss)
 
-        if print_loss:
-            loss = (-1) * loss / input_size
-            print(loss)
+            model = updateModel(model, A, H, batch_x, y_pred, batch_y)
 
+
+
+        # if print_loss:
+        #     loss = (-1) * loss / input_size
+        #     print(loss)
+        #     exit(0)
     return model
 
 
@@ -81,8 +83,8 @@ def updateModel(model, A, H, x, y_pred, y_vector):
     vdL_da = dL_da(A, model['W2'], vdL_dy)
     vdL_dw2 = dL_dw2(H, vdL_dy)
     vdL_dw1 = dL_dw1(x, vdL_da)
-    vdL_db2 = vdL_dy
-    vdL_db1 = vdL_da
+    vdL_db2 = np.average(vdL_dy)
+    vdL_db1 = np.average(vdL_da)
     eta = 0.05
     w1 = model["W1"] - np.multiply(eta, vdL_dw1)
     w2 = model["W2"] - np.multiply(eta, vdL_dw2)
@@ -93,7 +95,7 @@ def updateModel(model, A, H, x, y_pred, y_vector):
 
 
 def dL_dy(y_pred, y_vector):
-    return np.subtract(y_pred, y_vector)[np.newaxis]
+    return np.subtract(y_pred, y_vector)
 
 
 def dL_da(a, w2, vdl_dy):
@@ -101,8 +103,11 @@ def dL_da(a, w2, vdl_dy):
 
 
 def dL_dw2(h, dL_dy):
-    return np.squeeze(np.matmul(np.transpose(h[np.newaxis]), dL_dy))
+    # print((np.transpose(h), dL_dy))
+    # print((np.transpose(h).shape, dL_dy.shape))
+    # # ((3, 100, 1), (1, 100, 2))
+    return np.squeeze(np.matmul(np.transpose(h), dL_dy))
 
 
 def dL_dw1(x, dL_da):
-    return np.matmul(np.transpose(x[np.newaxis]), dL_da)
+    return np.squeeze(np.matmul(np.transpose(x), dL_da))
